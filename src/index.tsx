fb8352f4e101f7b6e427496809925797d338f5c3
khushboo <khushboo.panwar@hauper.com>
2023-06-15 15:11:14 +0530

    routing issue

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter ,HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
@@ -12,8 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 );
